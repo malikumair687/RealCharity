613cbb25d6e930cd04310a314e820452254a2e74
fareehaasghar487-cloud <fareehaasghar487@gmail.com>
2025-12-08 17:51:46 +0500

    code push

--- frontend/src/components/compaign/CampaignCard.jsx
+++ frontend/src/components/compaign/CampaignCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+ import React, { useEffect } from "react";
 import { FaBullseye, FaCheckCircle, FaCalendarAlt } from "react-icons/fa";
 import AOS from "aos";
 import "aos/dist/aos.css";
