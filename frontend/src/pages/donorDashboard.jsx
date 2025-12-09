f5cd8b755b5679f291827c1ef1688998455207f0
fareehaasghar487-cloud <fareehaasghar487@gmail.com>
2025-12-09 14:59:38 +0500

    UserController Changes

---
+++ frontend/src/pages/donorDashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from "../components/donordashboard/Dashboard";
+import DashboardCards from '../components/donordashboard/DashboardCards';
+const donorDashboard = () => {
+  return (
+    <>
+    <Dashboard/>
+    <DashboardCards/>
+    <Dash
+    </>
+  )
+}
+
+export default donorDashboard
